cacbcb273aa294b525dc2285a1a4db303db15027
LeapDev <lpdevstore@163.com>
2016-11-22 18:30:23 +0800

    .

---
+++ LPDevTools/Util/CommonMacro.h
@@ -0,0 +1,35 @@
+//
+//  CommonMacro.h
+//  LPDevTools
+//
+//  Created by lipeng on 16/11/22.
+//  Copyright © 2016年 lpdev.com. All rights reserved.
+//
+
+#ifndef CommonMacro_h
+#define CommonMacro_h
+
+#pragma mark 常用通知名称
+
+/**
+ *  新增或者修改作业的通知
+ */
+static NSString *homeWorkNotification = @"HomeWorkNotification";
+
+/**
+ *   新增设备的通知
+ */
+static NSString *classAcessAddNotification = @"ClassAcessAddNotification";
+
+
+
+
+#pragma mark 常用资源字符串
+
+/**
+ *  默认头像字符串
+ */
+static NSString *defaultAvatarString = @"mine_headimg";
+
+
+#endif /* CommonMacro_h */
