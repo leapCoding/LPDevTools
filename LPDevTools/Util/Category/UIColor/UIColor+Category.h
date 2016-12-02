6dd2366c778b8d7940d98bbfee022c7fa6f9c946
LeapDev <lpdevstore@163.com>
2016-12-02 18:28:36 +0800

    category

---
+++ LPDevTools/Util/Category/UIColor/UIColor+Category.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Category.h
+//  LPDevTools
+//
+//  Created by lipeng on 16/12/2.
+//  Copyright © 2016年 lpdev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Category)
+
+@end
---
+++ LPDevTools/Util/Category/UIColor/UIColor+Category.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Category.m
+//  LPDevTools
+//
+//  Created by lipeng on 16/12/2.
+//  Copyright © 2016年 lpdev.com. All rights reserved.
+//
+
+#import "UIColor+Category.h"
+
+@implementation UIColor (Category)
+
+@end
