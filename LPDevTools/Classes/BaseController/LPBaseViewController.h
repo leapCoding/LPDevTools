b8a4ea9688b211c43026953a417b1bb729eedb4c
leap <lpdevstore@163.com>
2018-04-28 16:27:13 +0800

    .

---
+++ LPDevTools/Classes/BaseController/LPBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPBaseViewController.h
+//  LPDevTools
+//
+//  Created by 李鹏 on 2018/4/28.
+//  Copyright © 2018年 lpdev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPBaseViewController : UIViewController
+
+@end
---
+++ LPDevTools/Classes/BaseController/LPBaseViewController.m
@@ -0,0 +1,30 @@
+//
+//  LPBaseViewController.m
+//  LPDevTools
+//
+//  Created by 李鹏 on 2018/4/28.
+//  Copyright © 2018年 lpdev.com. All rights reserved.
+//
+
+#import "LPBaseViewController.h"
+
+@interface LPBaseViewController ()
+
+@end
+
+@implementation LPBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
